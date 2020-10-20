eefeafa581046c0686af6d0cea03f88cf068c1d3
ShikharSundriyal <shikhar.sundriyal@gmail.com>
2020-10-20 17:39:31 +0530

    attaching java codes

---
+++ StacksQueues/BalancedBracket/BalancedBracket.java
@@ -0,0 +1,41 @@
+class BalancedBracket{
+public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		Stack<Character> st = new Stack();
+		for(int i =0; i < str.length(); i++){
+			char ch = str.charAt(i);
+			if(ch == '(' || ch == '[' || ch == '{'){
+					st.push();
+			}else if(ch == ')'){
+				if(st.size() > 0 && st.peek() == '('){
+					st.pop();
+				}else{
+					System.out.println("false");
+					return;
+				}
+			}else if(ch == ']'){
+				if(st.size() > 0 && st.peek() == '['){
+					st.pop();
+				}else{
+					System.out.println("false");
+					return;
+				}
+			}else if(ch == '{'){
+				if(st.size() > 0 && st.peek() == '}'){
+					st.pop();
+				}else{
+					System.out.println("false");
+					return;
+				}
+			}
+			
+		}
+		if(st.size() > 0 ){
+			System.out.println("false");
+		}else{
+			System.out.println("true");
+
+		}
+	}
+}
