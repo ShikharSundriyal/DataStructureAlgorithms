2e0eafab105c9ab004890d0a3bc7f9a425e98f41
ShikharSundriyal <shikhar.sundriyal@gmail.com>
2021-01-24 19:42:30 +0530

    Recursion

---
+++ Recursion/TargetSumSubset_Recursion/Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int tar = sc.nextInt();
+        printTargetSumSubsets(arr,0,"",0,tar);
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+		
+		// As in constraint all the elements of array are postivie once the sum so far becomes greater than target then we need not make any further calls for that as after that the sum will keep on increasing
+		if(sos > tar){
+            return;
+        }
+        if (idx == arr.length) {
+            if (sos == tar) {
+                System.out.println(set+".");
+            }
+            return;
+        }
+
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx]+", ", sos + arr[idx], tar);
+        printTargetSumSubsets(arr, idx + 1, set , sos , tar);
+    }
+
+}
