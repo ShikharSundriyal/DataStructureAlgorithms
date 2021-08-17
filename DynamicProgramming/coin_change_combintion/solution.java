d8e58f8136a1ec9a150c3e59964a8207f446b072
ShikharSundriyal <48173309+ShikharSundriyal@users.noreply.github.com>
2021-08-17 18:52:03 +0530

    Update solution.java

--- DynamicProgramming/coin_change_combintion/solution.java
+++ DynamicProgramming/coin_change_combintion/solution.java
@@ -15,13 +15,14 @@ public class Main {
     }
     
     public static int getCombiRecursive(int[] arr, int idx, int amt){
-        if( amt == 0) return 1;
-        if(amt < 0) return 0;
+        if( amt == 0) return 1; // using denominations present we have formed the amount
+        if(amt < 0) return 0; // as all the denominations are positive once amt becomes < 0 there is no possible way to form the amount 
         if( idx == arr.length){
-             return 0;
+             return 0; // if we have used all the denominations
         }
-        
+        // yes call, i.e. we have used the denomination at index idx at the next level also we can use the same denomination as we have infinite supply of coin but amt will get reduced for next level
         int y = getCombiRecursive(arr, idx, amt - arr[idx]);
+        // no call, i.e. we are not using current denomination at idx so prepare our amount so we can move to next idx but amount will remain same
         int n = getCombiRecursive(arr, idx+1, amt);
         return y + n;
     }
