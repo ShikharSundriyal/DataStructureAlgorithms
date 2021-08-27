7d8d92464e43685ee31f3757c6e2c845bb0fb782
ShikharSundriyal <48173309+ShikharSundriyal@users.noreply.github.com>
2021-08-27 19:19:55 +0530

    Update solution.java

--- DynamicProgramming/count a+b+c+ subsequence/solution.java
+++ DynamicProgramming/count a+b+c+ subsequence/solution.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        System.out.println(countTabulation(s));
+        // System.out.println(countTabulation(s));
+        System.out.println(countRecursive(s)[2]);
     }
     
     public static int countTabulation(String str){
@@ -25,5 +26,29 @@ public class Main {
             }
         }
         return abc;
+    }
+    
+    public static int[] countRecursive(String str){
+        
+        if(str.length() == 0)
+        {
+            int[] arr = new int[3];
+            return arr;
+        }
+        char ch = str.charAt(str.length()-1);
+        String sub = str.substring(0,str.length()-1);
+        int[] res = countRecursive(sub);
+        //res[0] -> a+
+        //res[1] -> a+b+
+        //res[2] -> a+b+c+
+        if(ch == 'a'){
+            res[0] = 2*res[0] + 1;
+        }else if(ch == 'b'){
+            res[1] = res[0] + 2*res[1];
+        }else if(ch == 'c'){
+            res[2] = res[1] + 2*res[2];
+        }
+        return res;
+        
     }
 }
