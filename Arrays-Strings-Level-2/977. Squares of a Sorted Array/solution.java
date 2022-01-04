db4a8ede4072130117bc74757ad84075d3db0c2e
ShikharSundriyal <48173309+ShikharSundriyal@users.noreply.github.com>
2022-01-04 18:25:12 +0530

    Create solution.java

---
+++ Arrays-Strings-Level-2/977. Squares of a Sorted Array/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] arr = new int[nums.length];
+        int i = 0,j=nums.length-1,k=nums.length-1;
+        while(i<=j){
+            if(nums[i]*nums[i] <=nums[j]*nums[j]){
+                arr[k]=nums[j]*nums[j];
+                j--;
+            }else{
+                arr[k]=nums[i]*nums[i];
+                i++;
+            }
+            k--;
+        }
+        return arr;
+    }
+}
